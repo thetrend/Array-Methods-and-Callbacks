d5fc90da95a6bae6acf8c1ad609989c6755dd391
Grace de la Mora <grace.g.delamora@gmail.com>
2021-03-19 21:28:02 -0400

    Finished task 4

--- index.js
+++ index.js
@@ -70,8 +70,17 @@ Use the higher-order function getWinners to do the following:
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
-    /* code here */
+function getWinners(array, getFinals) {
+    const finalsList = getFinals(array);
+    let winnersList = [];
+    finalsList.forEach((data) => {
+        if (data['Home Team Goals'] > data['Away Team Goals']) {
+            winnersList.push(data['Home Team Name']);
+        } else {
+            winnersList.push(data['Away Team Name']);
+        }
+    });
+    return winnersList;
 }
 
 
